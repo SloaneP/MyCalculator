0a6a2483c87f168ee5553907b61e36a607f78ef4
Sergey Fedorov <serezha-fedorov.01@mail.ru>
2023-09-08 17:35:02 +0300

    Реализовал меню программы

--- main.py
+++ main.py
@@ -34,3 +34,41 @@ def inp():
 def inp2():
     a = float(input("Input number: "))
     return a
+
+# main
+t = True
+while t:
+    num = int(input("Your choice! \n 1 - Add \n 2 - Subtract \n 3 - Divide \n 4 - Multiply \n 5 - Sqrt extraction \n 6 - Increase degree\n Input number: "))
+    
+    if num == 1:
+        a, b = inp()
+        print("Result Add: ", add(a, b))
+    elif num == 2:
+        a, b = inp()
+        print("Result Subtract: ", subtract(a, b))
+    elif num == 3:
+        a, b = inp()
+        print("Result Divide: ", divide(a, b))
+    elif num == 4:
+        a, b = inp()
+        print("Result Multiply: ", multiply(a, b))
+    elif num == 5:
+        a = inp2()
+        print("Result Sqrt: ", extract_sqrt(a))
+    elif num == 6:
+        a = inp2()
+        b = int(input("Input degree: "))
+        print("Result Inc. Degree: ", degree(a, b))
+    else:
+        print("Invalid argument!")
+        exit(-1)
+    
+    con = input("Would you like to continue? \n Input Y (yes) or N (no): ")
+    if con.lower() == 'y':
+        t = True
+    elif con.lower() == 'n':
+        t = False
+        break
+    else:
+        print("Invalid argument!")
+        exit(-1)
