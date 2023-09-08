a364aa71a5660796c988a13edd4a1b26d661fa1b
Sergey Fedorov <serezha-fedorov.01@mail.ru>
2023-09-08 17:34:18 +0300

    Реализовал основные функции

---
+++ main.py
@@ -0,0 +1,16 @@
+import math
+
+def multiply(a, b):
+    return a * b
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error! Second number is 0!"
