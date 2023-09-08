b9b1b9b3815764d0d02a6a0cb096b93229a073a8
Sergey Fedorov <serezha-fedorov.01@mail.ru>
2023-09-08 17:34:41 +0300

    Реализовал дополнительные функции

--- main.py
+++ main.py
@@ -14,3 +14,23 @@ def divide(a, b):
         return a / b
     else:
         return "Error! Second number is 0!"
+
+def extract_sqrt(a):
+    if a > 0:
+        return math.sqrt(a)
+    else:
+        return "Number is negative!"
+
+def degree(a, b):
+    # b - degree
+    # a - number
+    return a ** b
+
+def inp():
+    a = float(input("Input first number: "))
+    b = float(input("Input second number: "))
+    return a, b
+
+def inp2():
+    a = float(input("Input number: "))
+    return a
